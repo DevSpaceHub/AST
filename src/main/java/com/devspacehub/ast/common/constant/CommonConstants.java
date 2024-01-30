20a82e2db36caaef6fb0d8d55c84a5359182fc32
BananMoon <younji1115@gmail.com>
2024-01-30 11:21:40 +0900

    bugfix: 거래량 순위 데이터 파일 경로 서버 상 경로로 수정

--- src/main/java/com/devspacehub/ast/common/constant/CommonConstants.java
+++ src/main/java/com/devspacehub/ast/common/constant/CommonConstants.java
@@ -15,7 +15,7 @@ public class CommonConstants {
     public static final String REGISTER_ID = "application";
     public static final String OPENAPI_SUCCESS_RESULT_CODE = "0";
     public static final String ORDER_DIVISION = "00";   // 지정가
-    public static final String TRADING_VOLUME_RANKING_DATA_SAMPLE_JSON_PATH = "/Users/moonz/www/ast/trading-volume/sampleTradingVolumeGetData.json";
+    public static final String TRADING_VOLUME_RANKING_DATA_SAMPLE_JSON_PATH = "/www/ast/trading-volume/sampleTradingVolumeGetData.json";
 
     public static final String ACTIVE_LOCAL = "local";
     public static final String ACTIVE_BETA = "beta";
