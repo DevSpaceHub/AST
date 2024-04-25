a17ea7547bca99130457da12513cc67a8b768a56
BananMoon <younji1115@gmail.com>
2024-04-26 00:10:06 +0900

    refactor: 실행 중인 Profile 타입 및 계좌 상태에 대해 Enum으로 관리하도록 수정 - EnvironmentUtil 역할 이관 - 상수 클래스 내 관리되던 변수 제거

---
+++ src/test/java/com/devspacehub/ast/common/constant/ProfileTypeTest.java
@@ -0,0 +1,51 @@
+/*
+ © 2024 devspacehub, Inc. All rights reserved.
+
+ name : ProfileTypeTest
+ creation : 2024.4.25
+ author : Yoonji Moon
+ */
+
+package com.devspacehub.ast.common.constant;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileTypeTest {
+    @DisplayName("시스템 프로퍼티로 설정한 값을 가져와서 어떤 환경인지 확인한다.")
+    @Test
+    void getActiveProfile() {
+        //given
+        System.setProperty("spring.profiles.active", "local");
+        // when
+        String activeProfile = ProfileType.getActiveProfile();
+        // then
+        assertThat(activeProfile).isEqualTo("local");
+    }
+
+
+    @Test
+    @DisplayName("프로필이 'prod'인지 확인한다.")
+    void isProdActive() {
+        //given
+        System.setProperty("spring.profiles.active", "prod");
+        // when
+        Boolean isProdActive = ProfileType.isProdActive();
+        // then
+        assertThat(isProdActive).isTrue();
+    }
+
+    @DisplayName("프로필이 'prod'이면 계좌 상태는 '실전'을 반환한다.")
+    @Test
+    void getAccountStatus() {
+        //given
+        System.setProperty("spring.profiles.active", "prod");
+        // when
+        String accountStatus = ProfileType.getAccountStatus();
+        // then
+        assertThat(accountStatus).isEqualTo("실전");
+    }
+}
