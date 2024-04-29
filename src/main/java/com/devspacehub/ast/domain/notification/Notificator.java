ed80f91d347b19b3948f75b9021a1f341b1aecf9
BananMoon <younji1115@gmail.com>
2024-04-29 21:42:46 +0900

    refactor : 알림 메세지 DTO 추상화하여 공통 내용 추출.

--- src/main/java/com/devspacehub/ast/domain/notification/Notificator.java
+++ src/main/java/com/devspacehub/ast/domain/notification/Notificator.java
@@ -9,7 +9,8 @@ package com.devspacehub.ast.domain.notification;
 
 import com.devspacehub.ast.common.constant.OpenApiType;
 import com.devspacehub.ast.common.constant.ResultCode;
-import com.devspacehub.ast.domain.orderTrading.OrderTrading;
+import com.devspacehub.ast.domain.notification.dto.DiscordWebhookNotifyRequestDto;
+import com.devspacehub.ast.domain.notification.dto.MessageContentDto;
 import com.devspacehub.ast.exception.error.NotificationException;
 import com.devspacehub.ast.exception.error.InvalidValueException;
 import lombok.extern.slf4j.Slf4j;
@@ -21,7 +22,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import java.util.function.Consumer;
 
-import static com.devspacehub.ast.common.constant.CommonConstants.*;
 
 /**
  * Discord Webhook 이용한 알림 서비스
@@ -34,16 +34,12 @@ public class Notificator {
     private static final String ORDER_NOTI_SENDER_NAME = "주문 봇";
 
     /**
-     * 알림 요청
-     * @param openApiType
-     * @param activeProfile
-     * @param orderTrading
+     * 단일 메시지 발송 요청
+     * @param content 메세지 내용 DTO
      */
-    public void sendMessage(OpenApiType openApiType, String activeProfile, OrderTrading orderTrading) {
-        // body 생성
-        String senderName = getSenderName(openApiType);
-        String msg = createMessage(openApiType, getAccountStatus(activeProfile), orderTrading);
-        // API 호출
+    public <T extends MessageContentDto>  void sendMessage(T content) {
+        String senderName = getSenderName(content.getOpenApiType());
+        String msg = content.createMessage(content);
         Consumer<HttpHeaders> headers = DiscordWebhookNotifyRequestDto.setHeaders();
         DiscordWebhookNotifyRequestDto requestBody = DiscordWebhookNotifyRequestDto.builder()
                 .senderName(senderName)
@@ -62,22 +58,17 @@ public class Notificator {
                     .block();
 
         } catch (Exception ex) {
-            if (ex instanceof WebClientResponseException && HttpStatus.NO_CONTENT.equals(((WebClientResponseException) ex).getStatusCode())) {
+            if (ex instanceof WebClientResponseException webClientResponseException && HttpStatus.NO_CONTENT.equals(webClientResponseException.getStatusCode())) {
                 return;
             }
             log.error("response error: {}", ex.getMessage());
             throw new NotificationException();
         }
     }
-
-    private String getAccountStatus(String activeProfile) {
-        return ACTIVE_PROD.equals(activeProfile) ? REAL_ACCOUNT_STATUS_KOR : TEST_ACCOUNT_STATUS_KOR;
-    }
-
     /**
      * 알림 봇 이름 지정
-     * @param openApiType
-     * @return
+     * @param openApiType OpenApiType
+     * @return 알림 봇 이름
      */
     private String getSenderName(OpenApiType openApiType) {
         switch (openApiType) {
@@ -86,28 +77,6 @@ public class Notificator {
             }
             default -> throw new InvalidValueException(ResultCode.INVALID_OPENAPI_TYPE_ERROR);
         }
-    }
-
-    /**
-     * 알림 메시지 내용 작성
-     * @param openApiType
-     * @param accountStatusKor
-     * @param orderTrading
-     * @return
-     */
-    public String createMessage(OpenApiType openApiType, String accountStatusKor, OrderTrading orderTrading) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("**[").append(orderTrading.getOrderTime()).append("] ").append("주문완료**");
-        sb.append("\n계좌 상태 : ").append(accountStatusKor);
-        sb.append("\n종목명 : ").append(orderTrading.getItemNameKor()).
-                append(" (").append(orderTrading.getItemCode()).append(")");
-        sb.append("\n매매구분 : ").append(openApiType.getDiscription());
-        sb.append("\n주문수량 : ").append(orderTrading.getOrderQuantity()).append("주");
-        sb.append("\n주문단가 : ").append(orderTrading.getOrderPrice());
-        sb.append("\n주문번호 : ").append(orderTrading.getOrderNumber());
-
-        return sb.toString();
     }
 
 }
