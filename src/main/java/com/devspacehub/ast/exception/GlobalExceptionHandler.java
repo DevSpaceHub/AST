f24a8694fb31597c553177c143a2b4ce8ea482ec
BananMoon <younji1115@gmail.com>
2024-06-19 23:03:57 +0900

    feat : 예외 처리 방식 수정- super 예외클래스 호출 시 하위 예외 클래스의 정보를 잘 전달한다.

--- src/main/java/com/devspacehub/ast/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devspacehub/ast/exception/GlobalExceptionHandler.java
@@ -11,12 +11,16 @@ package com.devspacehub.ast.exception;
 import com.devspacehub.ast.common.dto.ApiResult;
 import com.devspacehub.ast.exception.error.BusinessException;
 import com.devspacehub.ast.common.constant.ResultCode;
+import com.devspacehub.ast.exception.error.InternalServerErrorException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * 전역에서 발생하는 Exception을 처리한다.
+ */
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,11 +33,28 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ApiResult.failed(resultCode));
     }
 
+    /**
+     * 고려하지 못한 Exception 발생 시 처리한다.
+     * @param ex Exception 타입 에러
+     * @return 500 상태코드 + 실패 ApiResult Dto 담은 ResponseEntity
+     */
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ApiResult> handleException(Exception ex) {
         log.error("handleException", ex);
 
         ResultCode resultCode = ResultCode.INTERNAL_SERVER_ERROR;
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResult.failed(resultCode));
+    }
+
+    /**
+     * InternalServerErrorException 문제 발생 시 처리한다.
+     * @param ex InternalServerErrorException을 상속받는 모든 Exception 클래스
+     * @return 500 상태코드 + 실패 ApiResult Dto 담은 ResponseEntity
+     */
+    @ExceptionHandler(InternalServerErrorException.class)
+    protected ResponseEntity<ApiResult> handleException(InternalServerErrorException ex) {
+        log.error("handleException", ex);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResult.failed(ex.getResultCode()));
     }
 }
--- src/main/java/com/devspacehub/ast/exception/error/BusinessException.java
+++ src/main/java/com/devspacehub/ast/exception/error/BusinessException.java
@@ -19,12 +19,21 @@ public class BusinessException extends RuntimeException {
     private final ResultCode resultCode;
 
     /**
-     * Instantiates a new Business exception.
-     *
-     * @param resultCode the error code
+     * BusinessException 하위 클래스 혹은 비즈니스 로직에서 호출하는 생성자
+     * @param resultCode 결과 코드
      */
     public BusinessException(ResultCode resultCode) {
-        super(resultCode.name() + " : " + resultCode.getMessage());
+        super(String.format("Code: %s (%s)", resultCode.name(), resultCode.getMessage()));
+        this.resultCode = resultCode;
+    }
+
+    /**
+     * BusinessException 하위 클래스 혹은 비즈니스 로직에서 호출하는 생성자
+     * @param resultCode 결과 코드
+     * @param message 에러 메세지
+     */
+    public BusinessException(ResultCode resultCode, String message) {
+        super(String.format("Code: %s (%s)%nException Message: %s", resultCode.name(), resultCode.getMessage(), message));
         this.resultCode = resultCode;
     }
 }
